3f1193ec12e31c0503ffded8837d702d2027c9a3
Nadeesha Sewwandi <157274511+Nadeesha200@users.noreply.github.com>
2025-05-06 13:05:29 +0530

    Update CommentController.java
    create post code changed

--- backend/src/main/java/net/javaguides/springboot/controller/CommentController.java
+++ backend/src/main/java/net/javaguides/springboot/controller/CommentController.java
@@ -23,7 +23,7 @@ public class CommentController {
 	@Autowired
 	private PostRepository postRepository;
 
-	// ✅ Create comment for a post
+	// Create comment for a post
 	@PostMapping("/posts/{postId}/comments")
 	public ResponseEntity<Comment> createComment(
 			@PathVariable Long postId,
