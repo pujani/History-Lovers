af4c72c6df705295a4c6de0a45fff9fd753d04d4
Nadeesha200 <157274511+Nadeesha200@users.noreply.github.com>
2025-05-06 13:01:01 +0530

    Moved frontend files into frontend/ folder

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HeaderComponent from './components/HeaderComponent';
+import CreatePostComponent from './components/CreatePostComponent';
+import UpdatePostComponent from './components/UpdatePostComponent';
+import ViewPostComponent from './components/ViewPostComponent';
+import PostListComponent from './components/PostListComponent';
+import UpdateCommentComponent from './components/UpdateCommentComponent'; // Make sure this import exists
+import ViewCommentComponent from './components/ViewCommentComponent';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            {/* Post Routes */}
+            <Route path="/posts" exact element={<PostListComponent />} />
+            <Route path="/add-post" element={<CreatePostComponent />} />
+            <Route path="/edit-post/:id" element={<UpdatePostComponent />} />
+            <Route path="/view-post/:id" element={<ViewPostComponent />} />
+
+            {/* Comment Routes - Nested under posts */}
+            <Route path="/posts/:id/edit-comment/:commentId" element={<UpdateCommentComponent />} /> {/* THIS LINE IS CRUCIAL */}
+            <Route path="/posts/:id/view-comment/:commentId" element={<ViewCommentComponent />} />
+
+            {/* Redirect root to posts */}
+            <Route path="/" exact element={<PostListComponent />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
