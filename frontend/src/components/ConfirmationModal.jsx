6a9a4bfa090fa9c49a0d867863f5656eee0519cf
Nadeesha Sewwandi <157274511+Nadeesha200@users.noreply.github.com>
2025-05-06 13:09:47 +0530

    Update ConfirmationModal.jsx
    confirmation model.jsx to pu class name and previous removed

--- frontend/src/components/ConfirmationModal.jsx
+++ frontend/src/components/ConfirmationModal.jsx
@@ -1,4 +1,4 @@
-// ConfirmationModal.jsx
+// This ConfirmationModal.jsx
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
